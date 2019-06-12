57c045fb137037d7bf9e118969701dab5a68f4ef
Pierre Haller <pierre@marmelab.com>
2019-06-12 13:49:20 +0200

    Add Game package

--- advisor-go/src/quixo/main.go
+++ advisor-go/src/quixo/main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"quixo/board"
 	"strconv"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 const port int = 8001
 
 func main() {
+	grid := [][]int{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+	newBoard := board.GetBoard(grid)
+	movables := board.GetMovablesCubes(newBoard)
+	spew.Dump(len(movables))
 	fmt.Printf("Server listening on %d", port)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
@@ -16,5 +29,5 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	w.Write([]byte(`{"message": "Hello world !"}`))
+	w.Write([]byte(`{"message": "Hello you !"}`))
 }
