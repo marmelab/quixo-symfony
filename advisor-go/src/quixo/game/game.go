ac6dc1b32b752c9befeb7a01139083c6692f37d9
Pierre Haller <pierre@marmelab.com>
2019-06-12 17:52:05 +0200

    Add simulations

--- advisor-go/src/quixo/game/game.go
+++ advisor-go/src/quixo/game/game.go
@@ -1,2 +1,167 @@
+package game
 
+import (
+	"fmt"
+	"strconv"
+)
 
+// Board with Grid and Player
+type Board struct {
+	Grid   [][]int
+	Player int
+}
+
+// Coords x and y
+type Coords struct {
+	X int
+	Y int
+}
+
+// Cube with Coords and value
+type Cube struct {
+	Coords Coords
+	Value  int
+}
+
+// GetBoard return a Board from grid and player
+func GetBoard(grid [][]int, player int) Board {
+	newBoard := Board{
+		Grid:   grid,
+		Player: player,
+	}
+	return newBoard
+}
+
+// GetMovablesCubes return an array of movables cube
+func GetMovablesCubes(board Board) []Cube {
+	grid := board.Grid
+	player := board.Player
+
+	movables := []Cube{}
+	for x := 0; x < len(grid); x++ {
+		for y := 0; y < len(grid[x]); y++ {
+			cube := Cube{
+				Coords: Coords{X: x, Y: y},
+				Value:  grid[x][y],
+			}
+			if isOutsideCube(cube.Coords, len(grid[x])) && isPlayerCube(cube.Value, player) {
+				movables = append(movables, cube)
+			}
+		}
+	}
+	return movables
+}
+
+// GetAvailablesDestinations return an array of destinations
+func GetAvailablesDestinations(grid [][]int, coords Coords) []Coords {
+	destinations := []Coords{}
+	x := coords.X
+	y := coords.Y
+	indexEnd := len(grid) - 1
+
+	if x == 0 || x == indexEnd {
+		if y != 0 {
+			destinations = append(destinations, Coords{X: x, Y: 0})
+		}
+		if y != indexEnd {
+			destinations = append(destinations, Coords{X: x, Y: indexEnd})
+		}
+		if x == 0 {
+			destinations = append(destinations, Coords{X: indexEnd, Y: y})
+		} else {
+			destinations = append(destinations, Coords{X: 0, Y: y})
+		}
+	} else if y == 0 || y == indexEnd {
+		if x != 0 {
+			destinations = append(destinations, Coords{X: 0, Y: y})
+		}
+		if x != indexEnd {
+			destinations = append(destinations, Coords{X: indexEnd, Y: y})
+		}
+		if y == 0 {
+			destinations = append(destinations, Coords{X: x, Y: indexEnd})
+		} else {
+			destinations = append(destinations, Coords{X: x, Y: 0})
+		}
+	}
+
+	return destinations
+}
+
+// MoveCube from coordsStart to coordsEnd
+func MoveCube(board Board, coordsStart Coords, coordsEnd Coords) [][]int {
+	newGrid := DuplicateGrid(board.Grid)
+	player := board.Player
+	if coordsStart.X == coordsEnd.X {
+		return shiftLine(newGrid, coordsStart.Y, coordsEnd.Y, coordsStart.X, player)
+	}
+	if coordsStart.Y == coordsEnd.Y {
+		flippedGrid := FlipRowsAndCols(newGrid)
+		flippedGrid = shiftLine(newGrid, coordsStart.X, coordsEnd.X, coordsStart.Y, player)
+		return FlipRowsAndCols(flippedGrid)
+	}
+	return nil
+}
+
+func isOutsideCube(coords Coords, size int) bool {
+	return coords.X == 0 || coords.Y == 0 || coords.X == size-1 || coords.Y == size-1
+}
+
+func isPlayerCube(cubeValue int, playerValue int) bool {
+	return cubeValue == 0 || cubeValue == playerValue
+}
+
+// Shift all value from xEnd to xStart in one row
+func shiftLine(grid [][]int, xStart int, xEnd int, rowIndex int, player int) [][]int {
+	newGrid := grid
+	step := 1
+	indexBound := xStart - 1
+	if xEnd > xStart {
+		step = -1
+		indexBound = xStart + 1
+	}
+	value := player
+	for index := xEnd; index != indexBound; index += step {
+		tmpValue := newGrid[rowIndex][index]
+		newGrid[rowIndex][index] = value
+		value = tmpValue
+	}
+	return newGrid
+}
+
+// FlipRowsAndCols return a 2d array with inverted cols and rows
+func FlipRowsAndCols(source [][]int) [][]int {
+	size := len(source)
+	flipped := make([][]int, size)
+	for i := 0; i < size; i++ {
+		flipped[i] = make([]int, size)
+	}
+	for x := 0; x < len(source); x++ {
+		for y := 0; y < len(source[x]); y++ {
+			flipped[y][x] = source[x][y]
+		}
+	}
+	return flipped
+}
+
+// DuplicateGrid return the copy of a 2D array
+func DuplicateGrid(grid [][]int) [][]int {
+	newGrid := make([][]int, len(grid))
+	for i := 0; i < len(grid); i++ {
+		newGrid[i] = make([]int, len(grid[i]))
+		copy(newGrid[i], grid[i])
+	}
+
+	return newGrid
+}
+
+// DrawBoard for debug purpose
+func DrawBoard(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			fmt.Printf(strconv.Itoa(grid[i][j]))
+		}
+		fmt.Printf("\n")
+	}
+	return 0
+}
--- advisor-go/src/quixo/scorer/scorer.go
+++ advisor-go/src/quixo/scorer/scorer.go
@@ -1,3 +1,5 @@
+package scorer
+
 import (
 	"quixo/game"
 )
@@ -5,23 +7,23 @@ import (
 /*
  * GetGridScore return the score of the board
  * the score of the board is the max score of all straight lines or diagonal
-*/
+ */
 func GetBoardScore(board game.Board) int {
-	diagonalScore = getDiagonalScore(board)
-	invertedDiagScore = getInvertedDiagonalScore(board)
-	rowsScore = getMaxLinesScore(board)
+	diagonalScore := getDiagonalScore(board)
+	invertedDiagScore := getInvertedDiagonalScore(board)
+	rowsScore := getMaxLinesScore(board)
 	invertedBoard := game.Board{
-		Board: board.FlipRowsAndCols(board.Grid)
-		Player: board.Player
+		Grid:   game.FlipRowsAndCols(board.Grid),
+		Player: board.Player,
 	}
-	colsScore = getMaxLinesScore(invertedBoard)
-	scores = []int{diagonalScore, invertedDiagScore, rowsScore, colsScore}
+	colsScore := getMaxLinesScore(invertedBoard)
+	scores := []int{diagonalScore, invertedDiagScore, rowsScore, colsScore}
 	return getMaxInArray(scores)
 }
 
 func getMaxLinesScore(board game.Board) int {
 	size := len(board.Grid)
-	scores := []int
+	scores := []int{}
 	for i := 0; i < size; i++ {
 		scores = append(scores, getLineScore(board.Grid[i], board.Player))
 	}
@@ -32,15 +34,15 @@ func getLineScore(line []int, player int) int {
 	score := 0
 	for i := 0; i < len(line); i++ {
 		if line[i] == player {
-			score += 1
+			score++
 		}
 	}
 	return score
 }
 
 func getDiagonalScore(board game.Board) int {
-	diag := []int
-	size = len(board.Grid)
+	diag := []int{}
+	size := len(board.Grid)
 	for i := 0; i < size; i++ {
 		diag = append(diag, board.Grid[i][i])
 	}
@@ -48,16 +50,16 @@ func getDiagonalScore(board game.Board) int {
 }
 
 func getInvertedDiagonalScore(board game.Board) int {
-	diag := []int
-	size = len(board.Grid)
+	diag := []int{}
+	size := len(board.Grid)
 	for i := 0; i < size; i++ {
-		diag = append(diag, board.Grid[i][size - 1 - i])
+		diag = append(diag, board.Grid[i][size-1-i])
 	}
 	return getLineScore(diag, board.Player)
 }
 
 func getMaxInArray(array []int) int {
-	max = 0
+	max := 0
 	for i := 0; i < len(array); i++ {
 		if array[i] > max {
 			max = array[i]
