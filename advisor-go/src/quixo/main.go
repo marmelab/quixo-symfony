f43b2a85dd069c98e2d971facafbb2cf3945c985
Pierre Haller <pierre@marmelab.com>
2019-06-12 17:52:05 +0200

    Add simulations

--- advisor-go/src/quixo/main.go
+++ advisor-go/src/quixo/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
+	"quixo/game"
+	"quixo/simulation"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 const port int = 8001
 
 func main() {
-	fmt.Printf("Server listening on %d", port)
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	grid := [][]int{
+		{0, 0, 0, 0, 1},
+		{0, 0, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+		{0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+	board := game.GetBoard(grid, 1)
+	bestMove := simulation.GetBestMove(board)
+	spew.Dump(bestMove)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	w.Write([]byte(`{"message": "Hello world !"}`))
+	w.Write([]byte(`{"message": "Hello you !"}`))
 }
