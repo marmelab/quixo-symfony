4e540dc1d0d00fbd24d5e139ba05fdb0962a746d
Pierre Haller <pierre@marmelab.com>
2019-06-14 15:57:09 +0200

    Add advice preview

--- app/src/Controller/AdviceController.php
+++ app/src/Controller/AdviceController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Manager\GameManager;
 use App\Manager\AdvisorManager;
+use App\Domain\Coords;
 
 class AdviceController extends AbstractController
 {
@@ -44,5 +45,26 @@ class AdviceController extends AbstractController
 
         $advice = $advisorManager->getWorstAdvice($game);
         return $this->json($advice);
+    }
+
+    public function previewAdvice(Request $request, GameManager $gameManager): Response
+    {
+        $id = $request->attributes->getInt('id');
+        $game = $gameManager->getGame($id);
+
+        $xStart = $request->attributes->getInt('xStart');
+        $yStart = $request->attributes->getInt('yStart');
+        $xEnd = $request->attributes->getInt('xEnd');
+        $yEnd = $request->attributes->getInt('yEnd');
+
+        $coordsStart = new Coords($xStart, $yStart);
+        $coordsEnd = new Coords($xEnd, $yEnd);
+
+        $newBoard = $gameManager->moveCube($game, $coordsStart, $coordsEnd, $game->getCurrentPlayer());
+        $game->setBoard($newBoard);
+
+        return $this->render('board.html.twig', [
+            'game' => $game
+        ]);
     }
 }
